2291e187ba519d9d9e3285d0d8694c8af7265f52
Jake Wehder <jw@email.comjake>
2022-09-08 15:54:30 -0500

    creating elements for the brewery info card

--- index.js
+++ index.js
@@ -13,6 +13,21 @@ document.querySelector('#search-form').addEventListener('submit', (e) => {
     searchForBrewery(e.target.query.value)
 })
 
+// DOM Manipulators
+
+// create elements for card
+function createBrewCard(breweryObj) {
+    // create a card with name, address, website, and phone number to append to the site
+    const breweryCollection = document.querySelector('#brewery-collection');
+    const div = document.createElement('div')
+    div.className = 'card';
+    const name = document.createElement('h3');
+    const phoneNumber = document.createElement('p')
+    const address = document.createElement('p')
+    const website = document.createElement('a')
+    const saveButton = document.createElement('button')
+}
+
 
 
 // Server Requests
@@ -25,6 +40,7 @@ function getBreweries() {
         // append values to page
 }
 
+// search for brewery based on query from search bar value
 function searchForBrewery(brewery) {
     fetch(`https://api.openbrewerydb.org/breweries/search?query=${brewery}`)
         .then(resp => resp.json())
