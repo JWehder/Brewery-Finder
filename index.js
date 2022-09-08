0c04a24d518a62013da360d9469fbe502f790259
Jake Wehder <jw@email.comjake>
2022-09-08 15:49:01 -0500

    adding event listener for search bar value

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-
+// Event Listeners
 
 // append breweries for page load
 document.addEventListener('DOMContentLoaded', () => {
@@ -6,4 +6,12 @@ document.addEventListener('DOMContentLoaded', () => {
     getSavedBreweriesLength()
 
 })
+
+// grab value from the search form and input it into brewery query
+document.querySelector('#search-form').addEventListener('submit', (e) => {
+    e.preventDefault()
+    searchForBrewery(e.target.query.value)
+})
+
+
 
